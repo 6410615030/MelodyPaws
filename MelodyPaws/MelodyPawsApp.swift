7406d76f7867c905f21667a353c1db4683a5c166
6410615030 <6410615030@student.tu.ac.th>
2024-04-28 17:52:37 +0700

    Initial Commit

---
+++ MelodyPaws/MelodyPawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelodyPawsApp.swift
+//  MelodyPaws
+//
+//  Created by Babypowder on 28/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct MelodyPawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
